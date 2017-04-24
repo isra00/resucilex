4cacd41cbbe329643572c27b0d87877df878d80e
isra00@gmail.com <isra00@gmail.com>
2017-04-24 23:28:30 +0300

    Take out translations from index; make / controller with auto redir depending on browser's Accept language

---
+++ translations.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+	'es' => [
+
+		//base.twig
+		'Lexicon of the He Rose From Death'
+		=> 'Léxico del Resucitó',
+		
+		'Lexicon <strong>He Rose</strong>'
+		=> 'Léxico <strong>Resucitó</strong>',
+
+		//home.twig
+		'Lexicon <small>of the</small> <strong>HE ROSE FROM DEATH</strong>'
+		=> 'Léxico <small>del</small> <strong>RESUCITÓ</strong>',
+		
+		'<strong>Notice</strong>: verbs appear in infinitive, and nouns in singular. Only single words are indexed, not composed terms. For instance, you will not find <em>word of God</em>, but <em>word</em> and <em>God</em> separately. Prepositions, pronouns, conjunctions, etc. are not indexed.'
+		=> '<strong>Nota</strong>: los verbos se listan en infinitivo, y los nombres y adjetivos, por lo general, en masculino singular. No se han indexado términos compuestos, sino palabras sueltas. Por ejemplo, no encontrarás <em>palabra de Dios</em>, sino <em>palabra</em> y <em>Dios</em> por separado. Preposiciones, pronombres, conjunciones, etc. no se listan.',
+
+		'The songs are taken from the songbook edited in <strong>London 2013</strong>.'
+		=> 'Los cantos están tomados del Resucitó editado en <strong>Madrid 2014</strong>.',
+
+		'Explore words <small>(%total%)</small>'
+		=> 'Explorar palabras <small>(%total%)</small>',
+
+		'Display list of words'
+		=> 'Ver lista de palabras',
+
+		'Display as summary'
+		=> 'Ver resumen con cantos',
+
+		'Display as word cloud'
+		=> 'Ver como nube de palabras',
+
+		//list.twig
+		'All words'
+		=> 'Todas las palabras',
+
+		'Totaling %total%'
+		=> '%total% en total',
+
+		//word.twig
+		'Songs where the word <strong>%word%</strong> appears:'
+		=> 'Cantos con la palabra <strong>%word%</strong>:',
+
+		'appears <strong>%total_occurences%</strong> times in <strong>%total_songs%</strong> song(s)'
+		=> 'Aparece <strong>%total_occurences%</strong> veces en <strong>%total_songs%</strong> canto(s)',
+
+		//summary.twig
+		'Summary'
+		=> 'Vista resumida',
+
+		//cloud.twig
+		'The more times each word occurs in the songs, the bigger it is displayed. Colors are just aesthetic.'
+		=> 'Las palabras se muestran más grandes cuantas más veces aparecen en los cantos. Los colores de las palabras son puramente decorativos: no significan nada.',
+	]
+];
--- web/index.php
+++ web/index.php
@@ -14,62 +14,7 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), array(
 
 $app->register(new \Silex\Provider\LocaleServiceProvider());
 $app->register(new \Silex\Provider\TranslationServiceProvider());
-
-$app['translator.domains'] = ['messages' => [
-	'es' => [
-
-		//base.twig
-		'Lexicon of the He Rose From Death'
-		=> 'Léxico del Resucitó',
-		
-		'Lexicon <strong>He Rose</strong>'
-		=> 'Léxico <strong>Resucitó</strong>',
-
-		//home.twig
-		'Lexicon <small>of the</small> <strong>HE ROSE FROM DEATH</strong>'
-		=> 'Léxico <small>del</small> <strong>RESUCITÓ</strong>',
-		
-		'<strong>Notice</strong>: verbs appear in infinitive, and nouns in singular. Only single words are indexed, not composed terms. For instance, you will not find <em>word of God</em>, but <em>word</em> and <em>God</em> separately. Prepositions, pronouns, conjunctions, etc. are not indexed.'
-		=> '<strong>Nota</strong>: los verbos se listan en infinitivo, y los nombres y adjetivos, por lo general, en masculino singular. No se han indexado términos compuestos, sino palabras sueltas. Por ejemplo, no encontrarás <em>palabra de Dios</em>, sino <em>palabra</em> y <em>Dios</em> por separado. Preposiciones, pronombres, conjunciones, etc. no se listan.',
-
-		'The songs are taken from the songbook edited in <strong>London 2013</strong>.'
-		=> 'Los cantos están tomados del Resucitó editado en <strong>Madrid 2014</strong>.',
-
-		'Explore words <small>(%total%)</small>'
-		=> 'Explorar palabras <small>(%total%)</small>',
-
-		'Display list of words'
-		=> 'Ver lista de palabras',
-
-		'Display as summary'
-		=> 'Ver resumen con cantos',
-
-		'Display as word cloud'
-		=> 'Ver como nube de palabras',
-
-		//list.twig
-		'All words'
-		=> 'Todas las palabras',
-
-		'Totaling %total%'
-		=> '%total% en total',
-
-		//word.twig
-		'Songs where the word <strong>%word%</strong> appears:'
-		=> 'Cantos en los que aparece la palabra <strong>%word%</strong>:',
-
-		'appears <strong>%total_occurences%</strong> times in <strong>%total_songs%</strong> song(s)'
-		=> 'Aparece <strong>%total_occurences%</strong> veces en <strong>%total_songs%</strong> canto(s)',
-
-		//summary.twig
-		'Summary'
-		=> 'Vista resumida',
-
-		//cloud.twig
-		'The more times each word occurs in the songs, the bigger it is displayed. Colors are just aesthetic.'
-		=> 'Las palabras se muestran más grandes cuantas más veces aparecen en los cantos. Los colores de las palabras son puramente decorativos: no significan nada.',
-	]
-]];
+$app['translator.domains'] = ['messages' => require __DIR__ . '/../translations.php'];
 
 $app->register(new \Silex\Provider\DoctrineServiceProvider(), array(
 	'db.options' => array(
@@ -84,13 +29,26 @@ $app->register(new \Silex\Provider\DoctrineServiceProvider(), array(
 
 $app['debug'] = $app['config']['debug'];
 
-/** @todo Ya que siempre ocurre, hacer un filtro de esos before siempre, pa no tener que declarar para cada route */
+
 $app->before(function (Request $req, Application $app) {
 	$locale = $app['db']->fetchColumn('SELECT locale FROM lang WHERE short = ?', [$app['locale']]);
 	setLocale(LC_COLLATE, $locale . '.utf8');
-	setLocale(LC_CTYPE, $locale . '.utf8');
+	setLocale(LC_CTYPE,   $locale . '.utf8');
 
 	$app['id_lang'] = $app['config']['lang'][$app['locale']];
+});
+
+
+$app->get('/', function(Request $req) use ($app)
+{
+	$acceptLang = $req->getPreferredLanguage(
+		array_column($app['config']['lang'], 'short')
+	);
+	
+	return $app->redirect($app['url_generator']->generate(
+		'home',
+		['_locale'=>$acceptLang]
+	));
 });
 
 $app->get('/{_locale}', function() use ($app) 
