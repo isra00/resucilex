415616d1843a010750d0db5ec05460cdaf9e3809
isra00 <isra00@gmail.com>
2025-03-09 10:07:04 +0100

    Base laravel app

--- dump.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config = require __DIR__ . '/config.php';
-
-exec("mysqldump -u{$config['db']['user']} -p{$config['db']['password']} {$config['db']['database']} lemma_review_es related --no-data > resucilex.sql");
-exec("mysqldump -u{$config['db']['user']} -p{$config['db']['password']} {$config['db']['database']} dufour lang lemma pos_code song word_song >> resucilex.sql");
